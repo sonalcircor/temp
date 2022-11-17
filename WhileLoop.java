db6712e2eca57033eea2e865b836f5e7150b415f
Sonal Patel <sonal@yahoo.co.in>
2022-11-17 14:07:21 +0000

    temp

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+package E.Forloops;
+
+public class WhileLoop {
+    public static void main(String args[]){
+        int i = 0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+
+        }
+    }
+}
---
+++ studentpercentage.java
@@ -0,0 +1,15 @@
+package E.Forloops;
+
+public class studentpercentage {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Manav");
+        System.out.println("Om");
+        System.out.println("Jevin");
+    }
+
+
+
+}
